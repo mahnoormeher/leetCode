3d15e07747133920932f51d201f8e68c8645b73f
mahnoormeher <168336224+mahnoormeher@users.noreply.github.com>
2024-09-04 22:01:38 +0500

    Time: 0 ms (100.00%), Space: 7.5 MB (67.75%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+     int sum=n;
+        while(sum!=1 && sum!=4){
+            int temp=sum;
+            sum=0;
+            while(temp!=0){
+                int digit=temp%10;
+                sum+=digit*digit;
+                temp=temp/10;
+            }
+        }
+        return sum==1;
+    }
+};
