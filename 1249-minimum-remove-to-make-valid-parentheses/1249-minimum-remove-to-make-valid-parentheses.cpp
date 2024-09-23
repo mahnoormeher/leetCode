91303e67c16a4dcf17a616d10ea71d358fcf2de7
mahnoormeher <168336224+mahnoormeher@users.noreply.github.com>
2024-09-24 00:51:55 +0500

    Time: 19 ms (75.58%), Space: 13.8 MB (37.86%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        int opencount=0;
+ int opencount=0;
         string result;
         for(char c:s){
             if(c=='('){
-                opencount++;
+                 opencount++;
             }else if(c==')'){
-                if(opencount==0){
-                    continue;
-                }
+                    if(opencount==0){
+                        continue;
+                    }
                     opencount--;
-               }
-       result+=c;
-        }
-        int closedcount=0;
-        string finalres;
-        for(int i=result.size()-1;i>=0;i--){
-            if(result[i]=='('){
-                if(closedcount==0){
-                    continue;
-                }
-                    closedcount--;
                 
-            }else if(result[i]==')'){
-                closedcount++;
             }
-          finalres+=result[i];
+                result+=c;
         }
-        reverse(finalres.begin(),finalres.end());
-        return finalres;
+            string finalres;
+            int closedcount=0;
+            for(int i=result.size()-1;i>=0;i--){
+                if(result[i]=='('){
+                    if(closedcount==0){
+                        continue;
+                    }
+                    closedcount--;
+                }
+                else if(result[i]==')'){
+                   
+                        closedcount++;
+                }
+                
+                finalres+=result[i];
+            }
+            reverse(finalres.begin(),finalres.end());
+            return finalres;
+        
+   
     }
 };
